9a11cdbd34fe4989754d8bbb8b3f87c4f5b50dda
M Ali R <maliramzan314@gmail.com>
2024-07-14 20:30:08 +0500

    first commit

---
+++ day1.js
@@ -0,0 +1,51 @@
+// Activity 1 : Variable Declaration
+
+// Task 1
+var number1 = 2;
+console.log(number1); // 2
+
+// Task 2
+let number2 = 3;
+console.log(number2); // 3
+
+// Activity 2: Constant Declaration
+
+// Task 3
+const boolean = true;
+console.log(boolean); // true
+
+//Activity 3: Datatypes
+let data1 = 24;
+console.log(typeof data1); // number
+
+let data2 = "Hello World";
+console.log(typeof data2); // string
+
+let data3 = true;
+console.log(typeof data3); //boolean
+
+let data4 = {
+  name: "M Ali R",
+  age: 18,
+};
+console.log(typeof data4); // Object
+
+let data5 = ["Football, Cricket, Tennis"];
+console.log(typeof data5); // Object
+
+//Activity 4: Reassigning Variables
+
+// Task 5
+let val1 = "Hello World!"; // initial value
+
+val1 = "Hello Guys!"; // Reassign Value
+console.log(val1);
+
+// Activity 5: Understanding const
+// Task 6
+
+const one = "Hello World!"; // Assign value to the constant
+
+// one = "Hello Guys!"; // Reassign Value
+
+console.log(one); // TypeError: Assignment to constant variable.
